11eb168c7b73f06d4f891b6e95110ac1c18cf7f4
Alex Mardis <mardistech@gmail.com>
2023-08-25 11:16:38 -0400

    added config and gitignore to practice hiding keys

--- weather.py
+++ weather.py
@@ -1,15 +1,17 @@
 import requests
+import config
 
-API_KEY = "1200342a464f1ee5062eba952d4f255a"
+
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
-request_url = f"{BASE_URL}?q={city}&APPID={API_KEY}"
+request_url = f"{BASE_URL}?q={city}&APPID={config.API_KEY}"
 response = requests.get(request_url)
 
 if response.status_code == 200:
     data = response.json()
-    print(f"In {city}, the temperature is {data['main']['temp']} kelvin, with {data['weather'][0]['description']}")
+    tempFahrenheit = round((((float(data['main']['temp']) - 273.15) *9) / 5) + 32, 2)
+    print(f"In {city} the temperature is {tempFahrenheit} Fahrenheit, with {data['weather'][0]['description']}")
 else:
     print("an error occured")
 
